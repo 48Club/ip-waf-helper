b096028398e0ec9ef003a6fac68fc1c13f3766f2
干净 <54979375+du5@users.noreply.github.com>
2024-08-20 00:37:30 +0800

    chore: Add script to add data from Cloudflare

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ip-waf-helper
 go 1.22.5
 
 require (
+	github.com/cloudflare/cloudflare-go v0.102.0
 	github.com/gin-gonic/gin v1.10.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.11
@@ -20,7 +21,7 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -38,7 +39,7 @@ require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ script/cloudflare/add_data_from_cloudflare.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"ip-waf-helper/database"
+	"ip-waf-helper/types"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+func main() {
+	apiKey := os.Getenv("CLOUDFLARE_API_KEY")
+	zone := os.Getenv("CLOUDFLARE_ZONE")
+	api, err := cloudflare.NewWithAPIToken(apiKey)
+	if err != nil {
+		panic(err)
+	}
+	page := 1
+	var rules = []cloudflare.AccessRule{}
+	var ruleIds = []string{}
+	tc := time.NewTicker((5 * time.Minute) / 1200)
+	for {
+		<-tc.C
+		time.After(100 * time.Millisecond)
+		rule, err := api.ListAccountAccessRules(context.Background(), zone, cloudflare.AccessRule{}, page)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			continue
+		}
+		rules = append(rules, rule.Result...)
+		if rule.ResultInfo.Page == rule.ResultInfo.TotalPages {
+			break
+		}
+		page++
+	}
+	for _, rule := range rules {
+		ip := rule.Configuration.Value
+		if rule.Configuration.Target == "ip_range" {
+			_, cidr64, _ := net.ParseCIDR(ip)
+			ip = cidr64.String()
+		}
+		line := types.IPWaf{
+			IP: ip,
+		}
+		tx := database.Server.FirstOrCreate(&line, line)
+		if tx.Error != nil {
+			panic(tx.Error)
+		}
+		ruleIds = append(ruleIds, rule.ID)
+	}
+	log.Printf("Done with %d rules", len(rules))
+	for _, v := range ruleIds {
+		<-tc.C
+		_, err := api.DeleteAccountAccessRule(context.Background(), zone, v)
+		if err != nil {
+			log.Printf("Error: %s", err)
+		}
+	}
+}
