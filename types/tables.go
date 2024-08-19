f5920df30f97841de59b2cac11d49a8ee28f0789
干净 <54979375+du5@users.noreply.github.com>
2024-08-19 19:50:44 +0800

    refactor: Add unique constraint to IP column in IPWaf table

--- types/tables.go
+++ types/tables.go
@@ -2,7 +2,7 @@ package types
 
 type IPWaf struct {
 	ID uint64 `gorm:"bigint;primaryKey;autoIncrement;column:id" json:"id"`
-	IP string `gorm:"varchar(255);column:ip" json:"ip" form:"ip" binding:"required"`
+	IP string `gorm:"varchar(255);unique;column:ip" json:"ip" form:"ip" binding:"required"`
 }
 
 func (IPWaf) TableName() string {
